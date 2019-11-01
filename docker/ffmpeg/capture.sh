df7cac0070173bba541fd0c25065dd9876f2b83d
Ivan Lychev <47522411+iLychevAD@users.noreply.github.com>
2019-11-01 23:05:43 +0300

    Overwrite captured image

--- docker/ffmpeg/capture.sh
+++ docker/ffmpeg/capture.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 while :
 do
-  ffmpeg -i rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov /capture/capture.jpg
+  ffmpeg -y -i rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov /capture/capture.jpg
   sleep 5
 done
   
