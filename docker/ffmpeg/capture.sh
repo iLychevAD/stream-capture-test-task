70bf6e2fd4c382432c48075de13756e6f140c2bd
Ivan Lychev <47522411+iLychevAD@users.noreply.github.com>
2019-11-01 23:14:54 +0300

    Fix capture command

--- docker/ffmpeg/capture.sh
+++ docker/ffmpeg/capture.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 while :
 do
-  ffmpeg -y -i rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov /capture/capture.jpg
+  ffmpeg -y -i rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov q:v 1 -update 1 /capture/capture.jpg
   sleep 5
 done
   
